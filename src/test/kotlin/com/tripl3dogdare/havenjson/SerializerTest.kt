7322753e38a69afe83ab591c36a1ae263183bd20
Connor Scialdone <connor@connorjulian.org>
2018-10-04 15:52:10 -0600

    Add test cases for JsonSchema.toJson

---
+++ src/test/kotlin/com/tripl3dogdare/havenjson/SerializerTest.kt
@@ -0,0 +1,73 @@
+package com.tripl3dogdare.havenjson
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.WordSpec
+
+class SerializerTest : WordSpec({
+  "JsonSchema#toJson" should {
+    "properly serialize basic types" {
+      BasicTypes(100, 100f, "test", false, null).toJson() shouldBe
+        Json("int" to 100, "float" to 100f, "string" to "test", "bool" to false, "nil" to null)
+    }
+
+    "properly serialize lists" {
+      Lists(listOf(100),
+            listOf(100f),
+            listOf("test"),
+            listOf(true, false),
+            listOf(null))
+      .toJson() shouldBe
+        Json("int" to listOf(100),
+             "float" to listOf(100f),
+             "string" to listOf("test"),
+             "bool" to listOf(true, false),
+             "nil" to listOf(null))
+    }
+
+    "properly serialize JSON types" {
+      JsonTypes(JsonInt(100),
+                JsonFloat(100f),
+                JsonString("test"),
+                JsonBoolean(false),
+                JsonNull,
+                JsonArray(listOf(JsonBoolean(true), JsonBoolean(false))),
+                JsonObject(mapOf("greeting" to JsonString("Hello"),
+                                 "target" to JsonString("world"))))
+      .toJson() shouldBe
+        Json("int" to 100,
+             "float" to 100f,
+             "string" to "test",
+             "bool" to false,
+             "nil" to null,
+             "arr" to listOf(true, false),
+             "obj" to mapOf("greeting" to "Hello",
+                            "target" to "world"))
+    }
+  }
+}) {
+  data class BasicTypes(
+    val int:Int,
+    val float:Float,
+    val string:String,
+    val bool:Boolean,
+    val nil:Nothing?
+  ) : JsonSchema
+
+  data class Lists(
+    val int:List<Int>,
+    val float:List<Float>,
+    val string:List<String>,
+    val bool:List<Boolean>,
+    val nil:List<Nothing?>
+  ) : JsonSchema
+
+  data class JsonTypes(
+    val int:JsonInt,
+    val float:JsonFloat,
+    val string:JsonString,
+    val bool:JsonBoolean,
+    val nil:JsonNull,
+    val arr:JsonArray,
+    val obj:JsonObject
+  ) : JsonSchema
+}
